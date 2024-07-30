d7d39214a259dc41ea4f55f07934d76688b19b05
Dimitris Sparagis <dimsparagis@gmail.com>
2024-07-30 18:49:15 +0300

    Created Input component to add fields dynamically

---
+++ src/components/Todo/TaskInput.jsx
@@ -0,0 +1,21 @@
+export const TaskInput = (props) => {
+    const changeHandler = (event) => {
+        props.onChange({
+            name: props.name,
+            value: event.target.value,
+        })
+    }
+
+    console.log(props.error);
+    return (
+        <div className={`flex flex-col gap-y-2 p-5`}>
+            <label htmlFor={props.name} className={`text-black text-2xl`}>{props.label}</label>
+            <input type={props.type} id={`${props.id}`} name={props.name}
+                   value={props.value}
+                   className={`border border-2 ${props.error ? 'bg-red-300 border-red-500' : 'bg-white border-gray-400'} rounded-md p-2 `}
+                   required={true}
+                   onChange={changeHandler}
+            />
+        </div>
+    );
+}
