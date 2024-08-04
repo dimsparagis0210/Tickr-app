260633b1f3118d96efaa7d3b5042fe77c83068bf
Dimitris Sparagis <dimsparagis@gmail.com>
2024-08-04 12:22:46 +0300

    Created a separate component for the New Task Window

---
+++ src/components/Todo/NewTaskWindow.jsx
@@ -0,0 +1,174 @@
+import {colors, inputs} from "../../data/data";
+import {NewTaskInput} from "./NewTaskInput";
+import {useEffect, useState} from "react";
+import {push, ref, update} from "firebase/database";
+import {useFirebase} from "../../hooks/useFirebase";
+import {useUserContext} from "../../store/user-context";
+
+export const NewTaskWindow = (props) => {
+    // States
+    const [error, setError] = useState({
+        name: 0,
+        description: 0,
+        deadline: 0,
+    });
+    const [submitted, setSubmitted] = useState(false);
+    const [selectedRadio, setSelectedRadio] = useState(null);
+    const [radio, setRadio] = useState({
+        high: false,
+        medium: false,
+        low: false,
+    });
+    const context = useUserContext();
+
+    // Effects
+    useEffect(() => {
+        if (submitted) {
+            setSelectedRadio(null); // Clear the selected option
+            setSubmitted(false); // Reset submitted flag
+        }
+    }, [submitted])
+
+    // Methods
+    const handleChange = ({name, value}) => {
+        if (value === "") {
+            console.log(`Please fill in the ${name} field`);
+            setError((prevState) => {
+                return (
+                    {
+                        ...prevState,
+                        [name]: 1,
+                    }
+                )
+            });
+        } else {
+            setError((prevState) => {
+                return {
+                    ...prevState,
+                    [name]: 0,
+                }
+            })
+
+        }
+        props.onChangeInput({name, value});
+    }
+
+    const setTime = () => {
+        const now = new Date();
+        const minutes = String(now.getMinutes()).padStart(2, '0'); // Pad with leading zero if needed
+        const formattedTime = `${now.getHours()}:${minutes}`;
+        return formattedTime;
+    }
+
+    //Function for the task submission
+    const submitTask = () => {
+        let areAllValid = 1;
+        setSubmitted(true);
+        const newTask = {
+            key: "",
+            name: props.input.name,
+            description: props.input.description,
+            deadline: props.input.deadline,
+            priority: props.input.priority,
+            status: "todo",
+            date: `${new Date()}`,
+            startTime: setTime(),
+        }
+        Object.keys(props.input).map((item) => {
+            if (props.input[item] === "" && item !== "description") {
+                setError((prevState) => {
+                    return {
+                        ...prevState,
+                        [item]: 1,
+                    }
+                })
+                areAllValid = 0;
+            }
+        });
+        if (!areAllValid) {
+            console.log("Please fill in all the fields");
+        } else {
+            props.onSaveTask(newTask);
+        }
+        console.log("Task submitted");
+    }
+    return (
+            props.taskWindowOpen &&
+                <>
+                    <div
+                        className={`z-10 self-end absolute rounded-xl shadow-2xl right-10 bg-gradient-to-tl from-violet-600 to-yellow-300 flex flex-col p-5`}
+                        style={{
+                            boxShadow: 'inset 0px 0px 0px 6px rgba(255,255,255,0.6)',
+                        }}
+                    >
+                        <button className={`bg-gradient-to-r from-red-50 to-zinc-100 h-fit 
+                                            rounded-xl shadow-xl hover:from-red-100 hover:to-zinc-200 
+                                            hover:shadow-2xl w-fit self-end
+                                            px-3 py-3 md:px-5 md:py-4 `
+                        } onClick={props.onToggle}>
+                            Close
+                        </button>
+                        <header>
+                            <h1 className={`text-xl md:text-3xl text-white p-10`}>Create a new task</h1>
+                        </header>
+                        <main>
+                            {
+                                Object.keys(inputs).map((item, index) => {
+                                    if (inputs[item].type !== "radio") {
+                                        return <NewTaskInput
+                                            id={inputs[item].id}
+                                            name={inputs[item].name}
+                                            type={inputs[item].type}
+                                            label={inputs[item].label}
+                                            placeholder={inputs[item].placeholder}
+                                            error={error[item]}
+                                            value={props.input[item]}
+                                            key={index}
+                                            onChange={({name, value}) => {
+                                                handleChange({name, value})
+                                            }}
+                                        />
+                                    } else {
+                                        return <div className={`flex flex-col gap-y-2 items-start p-2 md:p-5`}
+                                                    key={index}>
+                                            <label htmlFor={"priority"}
+                                                   className={`text-white text-lg md:text-2xl`}>Priority</label>
+                                            {
+                                                Object.keys(colors).map((color, index) => {
+                                                    return (
+                                                        <div className={`flex gap-4 text-${colors[index]}`} key={index}>
+                                                            <input type={"radio"} id={`radio`} name={"radio"}
+                                                                   value={color}
+                                                                   checked={selectedRadio === color}
+                                                                   className={`radioBtn border border-2 ${0 ?
+                                                                       'bg-red-300 border-red-500' :
+                                                                       'bg-white border-gray-400'} rounded-md p-2`}
+                                                                   required={true}
+                                                                   onClick={() => {
+                                                                       props.onChangePriority(colors[color]);
+                                                                   }}
+                                                                   onChange={() => {
+                                                                       setSelectedRadio(color);
+                                                                   }}
+                                                            />
+                                                            <label htmlFor="name">{color}</label>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                    }
+                                })
+                            }
+                        </main>
+                        <button className={`bg-gradient-to-r from-red-50 to-zinc-100 h-fit px-5 py-4 
+                                            rounded-xl shadow-xl hover:from-red-100 hover:to-zinc-200 
+                                            hover:shadow-2xl`}
+                                onClick={submitTask}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </>
+    )
+}
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -8,39 +8,24 @@ import {useNavigate} from "react-router-dom";
 import {useFirebase} from "../../hooks/useFirebase";
 import {push, ref, update} from "firebase/database";
 import {IsLoggedIn} from "../Auth/IsLoggedIn";
+import {Header} from "./Header";
+import {Background} from "./Background";
+import {NewTaskWindow} from "./NewTaskWindow";
 
 export const Todo = () => {
     //States
     const [taskWindowOpen, setTaskWindowOpen] = useState(false);
-    const [selectedRadio, setSelectedRadio] = useState(null);
+
     const [order, setOrder] = useState("date");
     const navigate = useNavigate();
-    const [radio, setRadio] = useState({
-        high: false,
-        medium: false,
-        low: false,
-    });
     const db = useFirebase();
     const context = useUserContext();
-    const [submitted, setSubmitted] = useState(false);
     const [input, setInput] = useState({
         name: "",
         description: "",
         deadline: "",
         priority: "",
     });
-    const [error, setError] = useState({
-        name: 0,
-        description: 0,
-        deadline: 0,
-    });
-
-    useEffect(() => {
-        if (submitted) {
-            setSelectedRadio(null); // Clear the selected option
-            setSubmitted(false); // Reset submitted flag
-        }
-    }, [submitted])
 
     //Getting user from local storage
     const user = JSON.parse(localStorage.getItem(context.email));
@@ -73,88 +58,6 @@ export const Todo = () => {
         }
         localStorage.setItem(user?.email, JSON.stringify(newUser));
     }, [tasks])
-
-    const setTime = () => {
-        const now = new Date();
-        const minutes = String(now.getMinutes()).padStart(2, '0'); // Pad with leading zero if needed
-        const formattedTime = `${now.getHours()}:${minutes}`;
-        return formattedTime;
-    }
-    //Function for the task submission
-    const submitTask = () => {
-        let areAllValid = 1;
-        setSubmitted(true);
-        const newTask = {
-            key: "",
-            name: input.name,
-            description: input.description,
-            deadline: input.deadline,
-            priority: input.priority,
-            status: "todo",
-            date: `${new Date()}`,
-            startTime: setTime(),
-        }
-        Object.keys(input).map((item) => {
-            if (input[item] === "" && item !== "description") {
-                setInput((prevState) => {
-                    return {
-                        ...prevState,
-                        error: 1,
-                    }
-                })
-                setError((prevState) => {
-                    return {
-                        ...prevState,
-                        [item]: 1,
-                    }
-                })
-                areAllValid = 0;
-            }
-        });
-        if (!areAllValid) {
-            console.log("Please fill in all the fields");
-        } else {
-            saveTask(context.id, newTask).then((key) => {
-                newTask.key = key;
-                setTasks((prevState) => {
-                    return [
-                        ...prevState,
-                        newTask,
-                    ]
-                });
-            }).then(() => {
-                const newUser = {
-                    ...user,
-                    tasks: tasks,
-                }
-                localStorage.setItem(user.email, JSON.stringify(newUser));
-                setInput({
-                    name: "",
-                    description: "",
-                    deadline: "",
-                    priority: "",
-                });
-                setRadio(false);
-            });
-        }
-        console.log("Task submitted");
-    }
-
-    const saveTask = (uid, task) => {
-        return new Promise((resolve, reject) => {
-            const newTaskKey = push(ref(db, `users/${uid}/tasks`)).key;
-            const updates = {};
-            updates[`users/${uid}/tasks/${newTaskKey}`] = task;
-
-            update(ref(db), updates)
-                .then(() => {
-                    resolve(newTaskKey);
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        });
-    }
 
     const completeHandler = (task, index) => {
         setTasks((prevTasks) =>
@@ -194,36 +97,6 @@ export const Todo = () => {
         });
     }
 
-    const handleChange = ({name, value}) => {
-        if (value === "") {
-            console.log(`Please fill in the ${name} field`);
-            setError((prevState) => {
-                return (
-                    {
-                        ...prevState,
-                        [name]: 1,
-                    }
-                )
-            });
-        } else {
-            setError((prevState) => {
-                return {
-                    ...prevState,
-                    [name]: 0,
-                }
-            })
-
-        }
-        setInput((prevState) => {
-            return (
-                {
-                    ...prevState,
-                    [name]: value,
-                }
-            )
-        });
-    }
-
     const logOutHandler = () => {
         localStorage.removeItem(context.email);
         console.log("Logged out");
@@ -238,106 +111,76 @@ export const Todo = () => {
         }
     }
 
+    const saveTask = (uid, task) => {
+        return new Promise((resolve, reject) => {
+            const newTaskKey = push(ref(db, `users/${uid}/tasks`)).key;
+            const updates = {};
+            updates[`users/${uid}/tasks/${newTaskKey}`] = task;
+
+            update(ref(db), updates)
+                .then(() => {
+                    resolve(newTaskKey);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    }
+
     return (
         <IsLoggedIn>
             <div className={`relative min-h-screen flex flex-col items-center justify-center`}>
-                <header className={`p-10 flex flex-col items-center`}>
-                    <h1 className={`text-4xl md:text-6xl text-gray-500`}>Welcome back Dimitris</h1>
-                    <h3 className={`text-xl md:text-2xl text-gray-400`}>Write down your tasks</h3>
-                </header>
-                <button className={`absolute  bg-gradient-to-r from-red-50 to-zinc-100 h-fit 
-                                            rounded-xl shadow-xl hover:from-red-100 hover:to-zinc-200 
-                                            hover:shadow-2xl w-fit text-sm md:text-md
-                                            px-4 py-3 md:px-5 md:py-4 
-                                            top-5 left-5 md:top-10 md:left-10
-                                  `}
-                        onClick={logOutHandler}
-                >
-                    Log Out
-                </button>
-                <div
-                    className={`absolute w-[24rem] md:w-[34rem] aspect-square rounded-full bg-purple-400 opacity-10 z-[-1] mt-[12rem]`}></div>
-                <div
-                    className={`absolute w-[14rem] md:w-[24rem] aspect-square rounded-full bg-pink-400 opacity-10 z-[-1] mt-[21rem]`}>
-                </div>
-                {
-                    taskWindowOpen &&
-                    <div
-                        className={`z-10 self-end absolute rounded-xl shadow-2xl right-10 bg-gradient-to-tl from-violet-600 to-yellow-300 flex flex-col p-5`}
-                        style={{
-                            boxShadow: 'inset 0px 0px 0px 6px rgba(255,255,255,0.6)',
-                        }}
-                    >
-                        <button className={`bg-gradient-to-r from-red-50 to-zinc-100 h-fit 
-                                            rounded-xl shadow-xl hover:from-red-100 hover:to-zinc-200 
-                                            hover:shadow-2xl w-fit self-end
-                                            px-3 py-3 md:px-5 md:py-4 `
-                        } onClick={() => setTaskWindowOpen(false)}>
-                            Close
-                        </button>
-                        <header>
-                            <h1 className={`text-xl md:text-3xl text-white p-10`}>Create a new task</h1>
-                        </header>
-                        <main>
-                            {
-                                Object.keys(inputs).map((item, index) => {
-                                    if (inputs[item].type !== "radio") {
-                                        return <NewTaskInput
-                                            id={inputs[item].id}
-                                            name={inputs[item].name}
-                                            type={inputs[item].type}
-                                            label={inputs[item].label}
-                                            placeholder={inputs[item].placeholder}
-                                            error={error[item]}
-                                            value={input[item]}
-                                            key={index}
-                                            onChange={({name, value}) => {
-                                                handleChange({name, value})
-                                            }}
-                                        />
-                                    } else {
-                                        return <div className={`flex flex-col gap-y-2 items-start p-2 md:p-5`} key={index}>
-                                            <label htmlFor={"priority"} className={`text-white text-lg md:text-2xl`}>Priority</label>
-                                            {
-                                                Object.keys(colors).map((color, index) => {
-                                                    return (
-                                                        <div className={`flex gap-4 text-${colors[index]}`} key={index}>
-                                                            <input type={"radio"} id={`radio`} name={"radio"}
-                                                                   value={color}
-                                                                   checked={selectedRadio === color}
-                                                                   className={`radioBtn border border-2 ${0 ?
-                                                                       'bg-red-300 border-red-500' :
-                                                                       'bg-white border-gray-400'} rounded-md p-2`}
-                                                                   required={true}
-                                                                   onClick={() => {
-                                                                       setInput((prevState) => ({
-                                                                           ...prevState,
-                                                                           priority: colors[color],
-                                                                       }))
-                                                                   }}
-                                                                   onChange={() => {
-                                                                         setSelectedRadio(color);
-                                                                   }}
-                                                            />
-                                                            <label htmlFor="name">{color}</label>
-                                                        </div>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-                                    }
-                                })
+                <Header onLogOut={logOutHandler}/>
+                <Background/>
+                <NewTaskWindow
+                    taskWindowOpen={taskWindowOpen}
+                    onToggle={() => {
+                        setTaskWindowOpen(false)
+                    }}
+                    input={input}
+                    user={user}
+                    onChangeInput={({name, value}) => {
+                        setInput((prevState) => {
+                            return (
+                                {
+                                    ...prevState,
+                                    [name]: value,
+                                }
+                            )
+                        });
+                    }}
+                    onSaveTask={(newTask) => {
+                        saveTask(context.id, newTask).then((key) => {
+                            newTask.key = key;
+                            setTasks((prevState) => {
+                                return [
+                                    ...prevState,
+                                    newTask,
+                                ]
+                            });
+                        }).then(() => {
+                            const newUser = {
+                                ...user,
+                                tasks: tasks,
                             }
-                        </main>
-                        <button className={`bg-gradient-to-r from-red-50 to-zinc-100 h-fit px-5 py-4 
-                                            rounded-xl shadow-xl hover:from-red-100 hover:to-zinc-200 
-                                            hover:shadow-2xl`}
-                                onClick={submitTask}
-                        >
-                            Submit
-                        </button>
-                    </div>
-                }
+                            localStorage.setItem(user.email, JSON.stringify(newUser));
+                            setInput({
+                                name: "",
+                                description: "",
+                                deadline: "",
+                                priority: "",
+                            });
+                        });
+                    }}
+                    onChangePriority={(priority) => {
+                        setInput((prevState) => {
+                            return {
+                                ...prevState,
+                                priority: priority,
+                            }
+                        });
+                    }}
+                />
                 <div className={`relative flex flex-col items-center rounded-xl shadow-2xl`}
                      style={{
                          background: "rgba(0, 0, 0, 0.1)",
